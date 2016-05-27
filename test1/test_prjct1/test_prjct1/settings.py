831a31826a78c4d730b9bc4cfac73d422d12f27c
swetha.naretla <swetha.naretla@micropyramid.com>
2016-05-27 17:33:42 +0530

    .

--- test1/test_prjct1/test_prjct1/settings.py
+++ test1/test_prjct1/test_prjct1/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'test_prjct1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': dj_database_url.config(
+       default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+   )
 }
 
 
